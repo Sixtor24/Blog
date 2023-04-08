6d1dc31e7416b1b4469e1b950c99674f09916ebd
Victor <vector6971@gmail.com>
2023-04-08 19:44:52 -0400

    sign in

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google";
+import DiscordProvider from "next-auth/providers/discord";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -12,9 +13,14 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
-    })
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET
+      })
     // ...add more providers here
     ],
+    secret: "hakjskasjhasj"
 }
 
 export default NextAuth(authOptions)
